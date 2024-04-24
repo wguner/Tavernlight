6673fa70a63541837549b18a088f85e497d55069
Setenay <setenayguner@gmail.com>
2024-04-24 16:17:24 -0700

    adding changes and comments to the method, renamed method to removeMemberParty

--- Q3.lua
+++ Q3.lua
@@ -1,11 +1,20 @@
-function do_sth_with_PlayerParty(playerId, membername)  
-    player = Player(playerId)  
-    local party = player:getParty()  
-      
-    for k,v in pairs(party:getMembers()) do  
-    if v == Player(membername) then  
-    party:removeMember(Player(membername))  
-    end  
-    end  
-    end  
-      
+     
+--[[
+This function removes a member from the party
+
+@param playerID : fetches the ID of the player. Party leader.
+@param memberName: Member to remove from the party.
+
+]]--
+function removeMemberFromParty(playerId, memberName)
+    -- CHANGE: creating a new player object and made it a local variable
+    local player = Player:new(playerId)
+    local party = player:getParty()
+    -- CHANGE: variable names to something more readable. 
+    for memberKey, dataValue in pairs(party:getMembers()) do
+        if dataValue == memberName then
+            party:removeMember(dataValue)
+            break -- exiting the loop so we don't go through the entire members in the party after we got rid of the user
+        end
+    end
+end
