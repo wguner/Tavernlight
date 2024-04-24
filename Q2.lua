f9b48932b9d7cd6d9275c73262a5698688a53afa
Setenay <setenayguner@gmail.com>
2024-04-24 12:53:36 -0700

    added error checking and fixing a bug of only printing one guild name

--- Q2.lua
+++ Q2.lua
@@ -1,7 +1,27 @@
-function printSmallGuildNames(memberCount)  
-    -- this method is supposed to print names of all guilds that have less than memberCount max members  
-    local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"  
-    local resultId = db.storeQuery(string.format(selectGuildQuery, memberCount))  
-    local guildName = result.getString("name")  
-    print(guildName)  
-    end  
+--[[
+Setenay Guner
+
+]]--
+
+function printSmallGuildNames(memberCount)
+    -- Assuming we have a valid database connection named 'db'
+    local selectGuildQuery = "SELECT name FROM guilds WHERE max_members < %d;"
+    -- Catching any errors with pcall https://www.lua.org/pil/8.4.html
+    local resultId, err = pcall(function()
+        return db.storeQuery(string.format(selectGuildQuery, memberCount))
+    end)
+    if not resultId then
+        print("Error executing query:", err)
+        return
+    end
+    
+    -- Before we were printing only one guild name
+    -- Writing a loop to print the result of the query 
+    -- Assuming db has fetchRow() method in that goes through the row
+    while db.fetchRow(resultId) do
+        local guildName = db.getString(resultId, "name")
+        print(guildName)
+    end
+end
+    
+
