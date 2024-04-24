6f30c2e24cc8c5cf581bc47005537228dfde5042
Setenay <setenayguner@gmail.com>
2024-04-24 11:33:39 -0700

    updated both methods with reasonings

--- Q1.lua
+++ Q1.lua
@@ -6,14 +6,15 @@
 --[[
 This method relesases storage of an associated player object
 @param player: object of player entity 
-@param storageValue: storage value to release
+@param storage: object of storage entity 
         REASON: Adding flexibility to accept a custom storage value. 
                 Assuming Storage is an object with its own methods getting/setting these values.
+                I do not like we are hard coding (1000) and (-1) values. These should be kept in a class and updated accordingly throughout events.
 --]]
-local function releaseStorage(player, storageValue) 
+local function releaseStorage(player, storage) 
     -- confirming player is a valid object
     if player then 
-        player:setStorageValue(storageValue, -1) 
+        player:setStorageValue(storage.value, storage.release) 
     -- if not, printing an error to the console in the call stack
     else
         error("invalid player object in releaseStorage")
@@ -26,10 +27,10 @@ This is an event handler.
 CHANGES: Got rid of the return value. This method is responsible for event handling we don't need to return anything.
          I do not like we're using hard-coded values like (1) and (-1) 
 @param player: object of player entity 
-@param storageValue: storage value -- REASON: adding flexibilty to accept a custom storage value. Assuming Storage is an object with its own methods getting/setting these values.
+@param storage: object of storage entity -- REASON: adding flexibilty to accept a custom storage value. Assuming Storage is an object with its own methods getting/setting these values.
 --]]
-function onLogout(player)  
-    if player:getStorageValue(storageValue) == 1 then  
-        addEvent(releaseStorage, 1000, player)  
+function onLogout(player, storage)  
+    if player:getStorageValue(storage.value) == storage.key then  
+        addEvent(releaseStorage, storage.value, player)  
     end  
 end  
